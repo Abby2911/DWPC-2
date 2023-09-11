935db0c4df83aa9daea41b69068c741dac1ae46f
MARKIGG <142839080+MARKIGG@users.noreply.github.com>
2023-09-11 16:40:14 +0000

    🌟Feature: Server Directory Se mueven los archivos del backend al directorio server

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //Crea un server de archivos estaticos 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'..', 'public')));
 
 //Registro de middlewares de aplicacion
 app.use('/', indexRouter);
