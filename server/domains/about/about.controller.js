33222e50efec5cb90174a6b7e7abc247eafbed45
MARKIGG <142839080+MARKIGG@users.noreply.github.com>
2023-10-27 05:37:46 +0000

    🌟Configuracion de puertos

--- server/domains/about/about.controller.js
+++ server/domains/about/about.controller.js
@@ -1,9 +1,10 @@
+// Actions methods
+
 // GET "/about"
 const about = (req, res) => {
   res.render('home/aboutView', { appVersion: '1.0.0' });
 };
 
-// Controlador Home
 export default {
   about,
 };
--- server/domains/home/home.controller.js
+++ server/domains/home/home.controller.js
@@ -3,13 +3,13 @@
 // GET "/index"
 const home = (req, res) => {
   const iconSet = ['⭐', '🤖', '🍉'];
-  const icon = iconSet[Math.floor(Math.random() * 3)];
+  const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
   res.render('home/homeView', { icon });
 };
 const about = (req, res) => {
   res.render('home/aboutView', { appVersion: '1.0.0' });
 };
-// Controlador Home
+
 export default {
   home,
   about,
