9a7930f337973a6456f925e848565620ab15cb1f
MARKIGG <142839080+MARKIGG@users.noreply.github.com>
2023-11-20 04:51:19 +0000

    ⭐ Feat: Persistencia de los proyectos

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,5 +1,7 @@
 // Importing winston logger
 import log from '../../config/winston';
+// Importando el modelo
+import ProjectModel from './project.model';
 // Actions methods
 // GET "/project"
 const showDashboard = (req, res) => {
@@ -10,7 +12,7 @@ const add = (req, res) => {
   res.render('project/addView');
 };
 // POST "/project/add"
-const addPost = (req, res) => {
+const addPost = async (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
   // En caso de haber error
@@ -33,10 +35,20 @@ const addPost = (req, res) => {
   // Se desestructura la información
   // de la peticion
   const { validData: project } = req;
-  // Se contesta la información
-  // del proyecto al cliente
-  log.info('Se entrega al cliente información del proyecto cargado');
-  return res.status(200).json(project);
+  // Creando la instancia de un documento
+  // con los valores de 'project'
+  try {
+    // Creando la instancia de un documento con los valores de 'project'
+    const savedProject = await ProjectModel.create(project);
+    // Se contesta la información del proyecto al cliente
+    log.info('Se entrega al cliente información del proyecto cargado');
+    return res.status(200).json(savedProject);
+  } catch (error) {
+    log.error(
+      'ln 53 project.controller: Error al guardar proyecto en la base de datos',
+    );
+    return res.status(500).json(error);
+  }
 };
 
 // Controlador user
