10c34645e2f4da914b71f7eb343c5120ffb1b25a
MARKIGG <142839080+MARKIGG@users.noreply.github.com>
2023-11-30 18:28:45 +0000

    🌟Fead: creando usuario y modelo usuario

--- server/domains/user/user.controller.js
+++ server/domains/user/user.controller.js
@@ -1,15 +1,20 @@
 import log from '../../config/winston';
+import User from './user.model';
+
 // Action Methods
+
 // GET '/user/login'
 const login = (req, res) => {
   // Sirve el formulario de login
   log.info('Se entrega formulario de login');
   res.render('users/login');
 };
+
 // GET '/user/logout'
 const logout = (req, res) => {
   res.send("🚧 UNDER CONSTRUCTION GET  '/user/logout' 🚧");
 };
+
 // GET '/user/register'
 const register = (req, res) => {
   log.info('Se entrega formulario de registro');
@@ -17,13 +22,29 @@ const register = (req, res) => {
 };
 
 // POST '/user/register'
-const registerPost = (req, res) => {
-  const { validData, errorData } = req;
+const registerPost = async (req, res) => {
+  const { validData: userFormData, errorData } = req;
   log.info('Se procesa formulario de registro');
-  res.json({
-    validData,
-    errorData,
-  });
+  // Verificando si hay errores
+  if (errorData) {
+    return res.json(errorData);
+  }
+  // En caso de no haber errores, se crea el usuario
+  try {
+    // 1. Se crea una instancia del modelo User
+    // mendiante la funcion create del modelo
+    const user = await User.create(userFormData);
+    log.info(`Usuario creado: ${JSON.stringify(user)}`);
+    // 3. Se contesta al cliente con el usuario creado
+    return res.status(200).json(user.toJSON());
+  } catch (error) {
+    log.error(error.message);
+    return res.json({
+      message: error.message,
+      name: error.name,
+      errors: error.errors,
+    });
+  }
 };
 
 export default {
--- server/domains/user/user.router.js
+++ server/domains/user/user.router.js
@@ -3,10 +3,13 @@ import { Router } from 'express';
 
 // Importando el controlador
 import userController from './user.controller';
+
 // Importando el validador del usuario
 import userValidator from './user.validator';
+
 // Importando el factory de validación
 import ValidateFactory from '../../services/validateFactory';
+
 // Creando una isntancia del enrutador
 const router = new Router();
 
@@ -16,11 +19,16 @@ router.get('/login', userController.login);
 
 // GET '/user/logout'
 router.get('/logout', userController.logout);
+
+// GET '/user/register'
+router.get('/register', userController.register);
+
 // POST '/user/register'
 router.post(
   '/register',
   ValidateFactory(userValidator.signUp),
   userController.registerPost,
 );
+
 // Exporto este tramo de ruta
 export default router;
